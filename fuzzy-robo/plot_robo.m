5f344b88e91efa61b41b1249128850df562f8e8e
Bruno Costa <doravante2@gmail.com>
2013-05-09 15:34:23 -0300

    Pequeno refactor para organização do código

---
+++ fuzzy-robo/plot_robo.m
@@ -0,0 +1,13 @@
+function plot_robo(x, y, raio)
+% Desenha o robo na tela
+% 	plot_robo(x,y, phi)
+%
+%  x = posicao do robo no mundo
+%  y = posicao do robo no mundo
+%  angulo = angulo do robo com a horizontal
+%  raio = raio do robo
+rectangle('Position',[x - raio y - raio 2*raio 2*raio],'Curvature',[1,1],'LineStyle',':','EdgeColor','r');
+plot(x, y, 'r+');
+%plot([x x + cos(angulo * pi / 180) * raio], [y y + sin(angulo * pi / 180) * raio], 'k');
+
+
--- fuzzy-robo/robo.m
+++ fuzzy-robo/robo.m
@@ -9,23 +9,23 @@ clear all
 clc
 
 %Raio do Robô
-raio_robo=6;
+raio_robo = 6;
 
 %Raio dos Obstáculos
-raio_obstaculo=3;
+raio_obstaculo = 3;
 
 %Número de Obstáculos
-num_obstaculos=10;
+num_obstaculos = 10;
 
 %Margem para iniciar os obstaculos
-margem_obstaculo=50;
+margem_obstaculo = 50;
 
 %Domínio: [0,200]x[0,100]
-yi=0;
-yf=100;
+yi = 0;
+yf = 100;
 
-xi=0;
-xf=200;
+xi = 0;
+xf = 200;
 
 delta = 2;
 
@@ -33,18 +33,18 @@ delta = 2;
 fis = readfis('funcoes');
 
 %Posição Inicial do Robô: Lado esquerdo do domínio
-centro_x_robo=raio_robo + 6; %robo encostado na extremidade esquerda do dominio 
-centro_y_robo=randi(yf - 2*raio_robo,1,1) + raio_robo;
+centro_x_robo = raio_robo + 6; %robo encostado na extremidade esquerda do dominio 
+centro_y_robo = randi(yf - 2*raio_robo,1,1) + raio_robo;
 
 %Ângulo Phi Inicial: -180<= phi <=180
-angulo_robo=randi(360,1,1) - 180;
+angulo_robo = randi(360,1,1) - 180;
 
 %Montando o Domínio e o Robô
 plot([0 1 1 0],[0 0 1 1]);
 axis([xi xf yi yf]);
-rectangle('Position',[centro_x_robo-raio_robo centro_y_robo-raio_robo 2*raio_robo 2*raio_robo],'Curvature',[1,1],'LineStyle',':','EdgeColor','r');
 hold on;
-plot(centro_x_robo, centro_y_robo, 'r+');
+
+plot_robo(centro_x_robo, centro_y_robo, raio_robo);
 
 x_obstaculo = zeros(num_obstaculos, 1);
 y_obstaculo = zeros(num_obstaculos, 1);
@@ -63,11 +63,11 @@ passo = 0;
 while( ~((centro_x_robo - raio_robo < xi) || centro_x_robo > xf || (centro_y_robo + raio_robo > yf) || (centro_y_robo - raio_robo < 0)) )
     distancias = ones(num_obstaculos + 1, 1) * 220;
     for i=1:num_obstaculos
-        bl1=centro_y_robo - tan(angulo_robo * pi / 180)*centro_x_robo;
-        bl2=y_obstaculo(i) - tan(angulo_robo * pi / 180 + pi/2)*x_obstaculo(i);
-        b=[bl1 bl2]';
-        a=[1 -tan(angulo_robo * pi / 180); 1 -tan(angulo_robo * pi / 180 + pi/2)];
-        Pint=a\b;
+        bl1 = centro_y_robo - tan(angulo_robo * pi / 180)*centro_x_robo;
+        bl2 = y_obstaculo(i) - tan(angulo_robo * pi / 180 + pi/2)*x_obstaculo(i);
+        b = [bl1 bl2]';
+        a = [1 -tan(angulo_robo * pi / 180); 1 -tan(angulo_robo * pi / 180 + pi/2)];
+        Pint = a\b;
 
         x_intersection = Pint(2);
         y_intersection = Pint(1);
@@ -84,16 +84,16 @@ while( ~((centro_x_robo - raio_robo < xi) || centro_x_robo > xf || (centro_y_rob
 
     plot([centro_x_robo centro_x_robo + cos(angulo_robo * pi / 180) * raio_robo], [centro_y_robo centro_y_robo + sin(angulo_robo * pi / 180) * raio_robo], 'k');
 
-    bl1=centro_y_robo - tan(angulo_robo * pi / 180)*centro_x_robo;
+    bl1 = centro_y_robo - tan(angulo_robo * pi / 180)*centro_x_robo;
 
     if angulo_robo > 0    
-        bl2=yf;
+        bl2 = yf;
     else
-        bl2=0;
+        bl2 = 0;
     end
-    b=[bl1 bl2]';
-    a=[1 -tan(angulo_robo * pi / 180); 1 0];
-    Pint=a\b;
+    b = [bl1 bl2]';
+    a = [1 -tan(angulo_robo * pi / 180); 1 0];
+    Pint = a\b;
 
     x_intersection = Pint(2);
     y_intersection = Pint(1);
@@ -111,12 +111,6 @@ while( ~((centro_x_robo - raio_robo < xi) || centro_x_robo > xf || (centro_y_rob
 
     %Calculo das novas posicoes
     %================================================================================
-    %Como as funcoes precisam dos angulos em radianos vou converter de graus
-    %para radianos
-    %
-    %    fprintf('Angulo atual    = %.2f\n', angulo);
-    %    fprintf('Posicao atual x = %.2f\n', posicao);
-    %    fprintf('Posicao atual y = %.2f\n', y);
     angulo_robo = angulo_robo + output;
     if angulo_robo < -90
         angulo_robo = angulo_robo + 360;
@@ -128,8 +122,7 @@ while( ~((centro_x_robo - raio_robo < xi) || centro_x_robo > xf || (centro_y_rob
     centro_x_robo = centro_x_robo + delta*cos(angulo_rad);
     centro_y_robo = centro_y_robo + delta*sin(angulo_rad);
 
-    rectangle('Position',[centro_x_robo-raio_robo centro_y_robo-raio_robo 2*raio_robo 2*raio_robo],'Curvature',[1,1],'LineStyle',':','EdgeColor','r');
-    plot(centro_x_robo, centro_y_robo, 'r+');
+    plot_robo(centro_x_robo, centro_y_robo, raio_robo);
     
     fprintf(1, 'Angulo atual = %.2f\n', angulo_robo);
     fprintf(1, 'Posicao atual X = %.2f\n', centro_x_robo);
